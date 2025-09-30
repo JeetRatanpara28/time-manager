35dfcb9ad3abb92029fa5a05c817b2bbca0a7f8f
Subhan Khaliq <subhankhaliq@Subhans-MacBook-Pro.local>
2025-09-30 16:19:03 +0200

    Dockerized the Application and fixed some issues mentioned in the last reveiw

--- time_manager_vue/vite.config.ts
+++ time_manager_vue/vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  // Load env file based on mode
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:4000',
+          changeOrigin: true
+        }
       }
     }
   }
